6cf695ad35a9ec1080fba4a55c3cc42dab8e8940
Nimira43 <n.rabone72@aol.com>
2024-12-08 21:49:57 +0000

    editor.tsx - setting up Tailwind styles.

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -6,11 +6,13 @@ import StarterKit from '@tiptap/starter-kit'
 export const Editor = () => {
   const editor = useEditor({
     extensions: [StarterKit],
-    content: '<p>Hello World! 🌎️</p>',
+    content: '<p>nt-Dox</p>',
   })
   return (
-    <div>
-      <EditorContent editor={editor}/>
+    <div className='size-full overflow-x-auto bg-[#f87144] px-4 print:p-0 print:bg-[#fff] print:overflow-visible'>
+      <div className='min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto'>
+        <EditorContent editor={editor}/>
+      </div>
     </div>
   )
 }
--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -8,8 +8,7 @@ const DocumentId = async ({ params }: DocumentIdPageProps) => {
   const { documentId } = await params
   
   return ( 
-    <div>
-      Document ID {documentId}
+    <div className='min-h-screen bg-[#fffaf0]'>
       <Editor />
     </div>
    )
