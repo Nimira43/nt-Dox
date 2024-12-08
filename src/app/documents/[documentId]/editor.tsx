af947fc0a1e0acf329c3d818d6e1fc087fa3f6b1
Nimira43 <n.rabone72@aol.com>
2024-12-08 22:06:07 +0000

    Styled the editor page attributes Editor page.

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -5,11 +5,16 @@ import StarterKit from '@tiptap/starter-kit'
 
 export const Editor = () => {
   const editor = useEditor({
+    editorProps: {
+      attributes: {
+        class: 'focus:outline-none print:border-0 bg-[#fff] border border-[#ccc] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text'
+      },
+    },
     extensions: [StarterKit],
     content: '<p>nt-Dox</p>',
   })
   return (
-    <div className='size-full overflow-x-auto bg-[#f87144] px-4 print:p-0 print:bg-[#fff] print:overflow-visible'>
+    <div className='size-full overflow-x-auto bg-[#ececec] px-4 print:p-0 print:bg-[#fff] print:overflow-visible'>
       <div className='min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0'>
         <EditorContent editor={editor}/>
       </div>
