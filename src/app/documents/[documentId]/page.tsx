6d51e2da7bd1a2829a227ae03e8306833c459da1
Nimira43 <n.rabone72@aol.com>
2024-12-08 19:24:20 +0000

    Setup routing for  documentId page

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,7 +1,14 @@
-const DocumentId = () => {
+interface DocumentIdPageProps {
+  params: Promise<{ documentId: string }>
+}
+
+const DocumentId = async ({ params }: DocumentIdPageProps) => {
+  const awaitedParams = await params
+  const documentId = awaitedParams.documentId
+
   return ( 
     <div>
-      Document ID
+      Document ID {documentId}
     </div>
    )
 }
