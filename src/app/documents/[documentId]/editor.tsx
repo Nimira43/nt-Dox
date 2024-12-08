4e4000a31765ae03937e4c1972ea53874cffe3d3
Nimira43 <n.rabone72@aol.com>
2024-12-08 21:52:03 +0000

    More Editor styling.

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -10,7 +10,7 @@ export const Editor = () => {
   })
   return (
     <div className='size-full overflow-x-auto bg-[#f87144] px-4 print:p-0 print:bg-[#fff] print:overflow-visible'>
-      <div className='min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto'>
+      <div className='min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0'>
         <EditorContent editor={editor}/>
       </div>
     </div>
